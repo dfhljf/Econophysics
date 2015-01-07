c8985864d2c546dd5deeb79d6f817b991612befe
dfhljf <jianghui19940917@hotmail.com>
2015-01-07 21:29:12 +0800

    使用新的图库画图，提升效率，图库还没修改完。需要对产生的文件进行压缩处理。

--- Econophysics/Graphic.cs
+++ Econophysics/Graphic.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using SvgNet;
-using SvgNet.SvgGdi;
+using Svg;
+using Svg.Pathing;
+using Svg.DataTypes;
 using CommonType;
 
 namespace Econophysics
@@ -14,7 +15,7 @@ namespace Econophysics
     {
         private int _width;
         private int _height;
-        private SvgGraphics _price;
+        //private SvgGraphics _price;
         private string _url;
         
         
@@ -23,7 +24,7 @@ namespace Econophysics
         {
             _width = 900;
             _height = 500;
-            _price = new SvgGraphics();
+            //_price = new SvgGraphics();
             _url = Experiment.Parameters.GraphicPart.Init.Url;
         }
         internal void Draw()
@@ -47,14 +48,18 @@ namespace Econophysics
             Point unitVector;
             int priceGapNumber;
             int basePrice;
-            List<double> priceList=Experiment._market.PriceList;
-            Pen blackPen = new Pen(Color.Black, 2);
-            Font smallFont = new Font(new FontFamily("Microsoft YaHei"), 16, FontStyle.Bold);
-            Font bigFont = new Font(new FontFamily("Microsoft YaHei"), 18);
+            List<double> priceList = new List<double>();
+            for (int i = 0; i < 50; i++)
+            {
+                priceList.Add(100 * Math.Sin(i));
+            }
+            SvgColourServer blackPen = new SvgColourServer(Color.Black);
+            SvgUnit smallFont = new SvgUnit(SvgUnitType.Point, 16);
+            SvgUnit bigFont = new SvgUnit(SvgUnitType.Point, 18);
+            string fontFamily = "微软雅黑";
             SolidBrush blackBrush = new SolidBrush(Color.Black);
-            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SvgColourServer redBrush = new SvgColourServer(Color.Red);
 
-            _price.Clear(Color.Transparent);
             //获取最大最小价格
             priceRange[0] = priceList[0];
             priceRange[1] = priceList[0];
@@ -74,75 +79,170 @@ namespace Econophysics
             }
             unitVector = new Point(-(_width - 57) / (priceList.Count - 1), -(_height - 50) / priceGapNumber);
 
-            //生成价格点
-            Point[] pricePoints = new Point[priceList.Count];
-            for (int i = 0; i < priceList.Count; i++)
-            {
-                pricePoints[i] = new Point(origin.X + i * unitVector.X, Convert.ToInt32(origin.Y + (priceList[priceList.Count - 1 - i] - basePrice) * unitVector.Y));
-            }
+            SvgDocument s = new SvgDocument { Height = 500, Width = 900 };
+            var group = new SvgGroup();
+            s.Children.Add(group);
 
-            //参考线 Price=100
-            //priceGraphics.DrawLine(new Pen(Color.Black, 2), new Point(0, originPoint.Y), new Point(originPoint.X, originPoint.Y));
             //纵坐标
-            _price.DrawLine(blackPen, new Point(origin.X, 0), new Point(origin.X, _height));
-            ////横坐标
-            _price.DrawLine(blackPen, new Point(0, origin.Y - priceGapNumber * unitVector.Y / 2), new Point(_width, origin.Y - priceGapNumber * unitVector.Y / 2));
-            _price.DrawString("价格", smallFont, blackBrush, new Point(origin.X, origin.Y - priceGapNumber * unitVector.Y / 2 + 25));
+            group.Children.Add(new SvgLine { StartX = origin.X, StartY = 0, EndX = origin.X, EndY = _height, Stroke = blackPen, StrokeWidth = 2 });
+            //横坐标
+            group.Children.Add(new SvgLine { StartX = 0, StartY = origin.Y - priceGapNumber * unitVector.Y / 2, EndX = _width, EndY = origin.Y - priceGapNumber * unitVector.Y / 2, Stroke = blackPen, StrokeWidth = 2 });
+            group.Children.Add(new SvgText { Text = "价格", FontFamily = fontFamily, FontSize = smallFont, FontWeight = SvgFontWeight.bold, X = origin.X, Y = origin.Y - priceGapNumber * unitVector.Y / 2 + 25 });
             //纵坐标标记
             {
                 int tmp = priceGapNumber / 8;
                 tmp = tmp + 5 - tmp % 5;
                 for (int i = 0; i <= priceGapNumber / 2; i++)
                 {
-                    _price.DrawLine(blackPen, new Point(origin.X - (i % 5 == 0 ? 8 : 4), origin.Y - i * unitVector.Y), new Point(origin.X, origin.Y - i * unitVector.Y));
-                    _price.DrawLine(blackPen, new Point(origin.X - (i % 5 == 0 ? 8 : 4), origin.Y + i * unitVector.Y), new Point(origin.X, origin.Y + i * unitVector.Y));
+                    group.Children.Add(new SvgLine { StartX = origin.X - (i % 5 == 0 ? 8 : 4), StartY = origin.Y - i * unitVector.Y, EndX = origin.X, EndY = origin.Y - i * unitVector.Y, Stroke = blackPen, StrokeWidth = 2 });
+                    group.Children.Add(new SvgLine { StartX = origin.X - (i % 5 == 0 ? 8 : 4), StartY = origin.Y + i * unitVector.Y, EndX = origin.X, EndY = origin.Y + i * unitVector.Y, Stroke = blackPen, StrokeWidth = 2 });
+
                     if (i % tmp == 0 && i != priceGapNumber / 2 && i != 0)
                     {
-                        _price.DrawString((basePrice + i).ToString(), bigFont, blackBrush, new Point(origin.X + 2, origin.Y + i * unitVector.Y + 10));
-                        _price.DrawString((basePrice - i).ToString(), bigFont, blackBrush, new Point(origin.X + 2, origin.Y - i * unitVector.Y + 10));
+                        group.Children.Add(new SvgText { Text = (basePrice + i).ToString(), FontFamily = fontFamily, FontSize = bigFont, X = origin.X + 2, Y = origin.Y + i * unitVector.Y + 10 });
+                        group.Children.Add(new SvgText { Text = (basePrice - i).ToString(), FontFamily = fontFamily, FontSize = bigFont, X = origin.X + 2, Y = origin.Y - i * unitVector.Y + 10 });
                     }
                     else if (i == 0)
                     {
-                        _price.DrawString(basePrice.ToString(), bigFont, blackBrush, new Point(origin.X + 2, origin.Y + i * unitVector.Y + 10));
+                        group.Children.Add(new SvgText { Text = basePrice.ToString(), FontFamily = fontFamily, FontSize = bigFont, X = origin.X + 2, Y = origin.Y + 10 });
                     }
                 }
             }
-
             //价格线
-            _price.DrawLines(new Pen(Color.Blue, 2), pricePoints);
+            SvgPolyline pricePath = new SvgPolyline { Fill = SvgPaintServer.None, Stroke = new SvgColourServer(Color.Blue), StrokeWidth = 2, Points = new SvgUnitCollection() };
+            group.Children.Add(pricePath);
+            for (int i = 1; i <= priceList.Count; i++)
+            {
+                pricePath.Points.Add(origin.X + i * unitVector.X);
+                pricePath.Points.Add((Single)(origin.Y + (priceList[priceList.Count - i] - basePrice) * unitVector.Y));
+            }
 
             //标记点和价格
+            for (int i = 0; i < pricePath.Points.Count / 2; i++)
             {
-                Point point;
-                for (int i = 0; i < pricePoints.Length; i++)
+                if (i != pricePath.Points.Count / 2 - 1)
                 {
-                    point = pricePoints[i];
-
-                    if (i != pricePoints.Length - 1)
-                    {
-                        if (i != 0)//以前价格点
-                            _price.FillEllipse(redBrush, Convert.ToInt32(point.X - 3), Convert.ToInt32(point.Y - 3), 6, 6);
-                        else//现在价格点
-                            _price.FillEllipse(redBrush, Convert.ToInt32(point.X - 8), Convert.ToInt32(point.Y - 8), 16, 16);
-                        if (i % 5 == 0 && i != 0)//横坐标标记
-                            _price.DrawString(i.ToString(), bigFont, blackBrush, new Point(origin.X + i * unitVector.X - 12, origin.Y - priceGapNumber * unitVector.Y / 2 + 25));
-                    }
-                    else
-                    {
-                        _price.DrawString("轮次", smallFont, blackBrush, new Point(origin.X + i * unitVector.X - 12, origin.Y - priceGapNumber * unitVector.Y / 2 + 25));
-                    }
-
-                    //横坐标线
-                    _price.DrawLine(blackPen, new Point(origin.X + i * unitVector.X, origin.Y - priceGapNumber * unitVector.Y / 2), new Point(origin.X + i * unitVector.X, origin.Y - priceGapNumber * unitVector.Y / 2 - (i % 5 == 0 ? 8 : 4)));
+                    if (i != 0)//以前价格点
+                        group.Children.Add(new SvgEllipse { CenterX = pricePath.Points[2 * i] - 3, CenterY = pricePath.Points[2 * i + 1] - 3, RadiusX = 6, RadiusY = 6, Fill = redBrush, Stroke = SvgPaintServer.None });
+                    else//现在价格点
+                        group.Children.Add(new SvgEllipse { CenterX = pricePath.Points[2 * i] - 8, CenterY = pricePath.Points[2 * i + 1] - 8, RadiusX = 16, RadiusY = 16, Fill = redBrush, Stroke = SvgPaintServer.None });
+                    if (i % 5 == 0 && i != 0)//横坐标标记
+                        group.Children.Add(new SvgText { Text = i.ToString(), FontFamily = fontFamily, FontSize = bigFont, X = origin.X + i * unitVector.X - 12, Y = origin.Y - priceGapNumber * unitVector.Y / 2 + 25 });
                 }
+                else
+                {
+                    group.Children.Add(new SvgText { Text = "轮次", FontFamily = fontFamily, FontSize = smallFont, FontWeight = SvgFontWeight.bold, X = origin.X + i * unitVector.X - 12, Y = origin.Y - priceGapNumber * unitVector.Y / 2 + 25 });
+                }
+
+                //横坐标线
+                group.Children.Add(new SvgLine { StartX = origin.X + i * unitVector.X, StartY = origin.Y - priceGapNumber * unitVector.Y / 2, EndX = origin.X + i * unitVector.X, EndY = origin.Y - priceGapNumber * unitVector.Y / 2 - (i % 5 == 0 ? 8 : 4), Stroke = blackPen, StrokeWidth = 2 });
             }
             //本轮价格
-            _price.DrawString(string.Format("本轮价格：{0:F2}", priceList[priceList.Count - 1]), new Font(new FontFamily("Microsoft YaHei"), 20, FontStyle.Bold), blackBrush, new PointF(origin.X + priceList.Count * unitVector.X / 2 - 100, origin.Y - 200));
+            group.Children.Add(new SvgText { Text = string.Format("本轮价格：{0:F2}", priceList[priceList.Count - 1]), FontFamily = fontFamily, FontSize = new SvgUnit(SvgUnitType.Point, 20), FontWeight = SvgFontWeight.bold, X = origin.X + priceList.Count * unitVector.X / 2 - 100, Y = origin.Y - 200 });
         }
+        //private void oldDraw()
+        //{
+        //    double[] priceRange = new double[2];
+        //    Point origin = new Point(_width - 50, _height / 2 - 30 / 2 + 20 / 2);
+        //    Point unitVector;
+        //    int priceGapNumber;
+        //    int basePrice;
+        //    List<double> priceList=Experiment._market.PriceList;
+        //    Pen blackPen = new Pen(Color.Black, 2);
+        //    Font smallFont = new Font(new FontFamily("Microsoft YaHei"), 16, FontStyle.Bold);
+        //    Font bigFont = new Font(new FontFamily("Microsoft YaHei"), 18);
+        //    SolidBrush blackBrush = new SolidBrush(Color.Black);
+        //    SolidBrush redBrush = new SolidBrush(Color.Red);
+
+        //    _price.Clear(Color.Transparent);
+        //    //获取最大最小价格
+        //    priceRange[0] = priceList[0];
+        //    priceRange[1] = priceList[0];
+        //    for (int i = 1; i < priceList.Count; i++)
+        //    {
+        //        priceRange[0] = (priceList[i] > priceRange[0] ? priceList[i] : priceRange[0]);
+        //        priceRange[1] = (priceList[i] < priceRange[1] ? priceList[i] : priceRange[1]);
+        //    }
+
+        //    //获取尺度
+        //    {
+        //        int tmp = Convert.ToInt32(priceRange[0] - priceRange[1]);
+        //        priceGapNumber = (tmp + tmp % 2 + 10);
+        //        priceGapNumber = (priceGapNumber > 20 ? priceGapNumber : 20);
+        //        tmp = Convert.ToInt32((priceRange[0] + priceRange[1]) / 2);
+        //        basePrice = tmp + (tmp % 5 > 2 ? 5 - tmp % 5 : -tmp % 5);
+        //    }
+        //    unitVector = new Point(-(_width - 57) / (priceList.Count - 1), -(_height - 50) / priceGapNumber);
+
+        //    //生成价格点
+        //    Point[] pricePoints = new Point[priceList.Count];
+        //    for (int i = 0; i < priceList.Count; i++)
+        //    {
+        //        pricePoints[i] = new Point(origin.X + i * unitVector.X, Convert.ToInt32(origin.Y + (priceList[priceList.Count - 1 - i] - basePrice) * unitVector.Y));
+        //    }
+
+        //    //参考线 Price=100
+        //    //priceGraphics.DrawLine(new Pen(Color.Black, 2), new Point(0, originPoint.Y), new Point(originPoint.X, originPoint.Y));
+        //    //纵坐标
+        //    _price.DrawLine(blackPen, new Point(origin.X, 0), new Point(origin.X, _height));
+        //    ////横坐标
+        //    _price.DrawLine(blackPen, new Point(0, origin.Y - priceGapNumber * unitVector.Y / 2), new Point(_width, origin.Y - priceGapNumber * unitVector.Y / 2));
+        //    _price.DrawString("价格", smallFont, blackBrush, new Point(origin.X, origin.Y - priceGapNumber * unitVector.Y / 2 + 25));
+        //    //纵坐标标记
+        //    {
+        //        int tmp = priceGapNumber / 8;
+        //        tmp = tmp + 5 - tmp % 5;
+        //        for (int i = 0; i <= priceGapNumber / 2; i++)
+        //        {
+        //            _price.DrawLine(blackPen, new Point(origin.X - (i % 5 == 0 ? 8 : 4), origin.Y - i * unitVector.Y), new Point(origin.X, origin.Y - i * unitVector.Y));
+        //            _price.DrawLine(blackPen, new Point(origin.X - (i % 5 == 0 ? 8 : 4), origin.Y + i * unitVector.Y), new Point(origin.X, origin.Y + i * unitVector.Y));
+        //            if (i % tmp == 0 && i != priceGapNumber / 2 && i != 0)
+        //            {
+        //                _price.DrawString((basePrice + i).ToString(), bigFont, blackBrush, new Point(origin.X + 2, origin.Y + i * unitVector.Y + 10));
+        //                _price.DrawString((basePrice - i).ToString(), bigFont, blackBrush, new Point(origin.X + 2, origin.Y - i * unitVector.Y + 10));
+        //            }
+        //            else if (i == 0)
+        //            {
+        //                _price.DrawString(basePrice.ToString(), bigFont, blackBrush, new Point(origin.X + 2, origin.Y + i * unitVector.Y + 10));
+        //            }
+        //        }
+        //    }
+
+        //    //价格线
+        //    _price.DrawLines(new Pen(Color.Blue, 2), pricePoints);
+
+        //    //标记点和价格
+        //    {
+        //        Point point;
+        //        for (int i = 0; i < pricePoints.Length; i++)
+        //        {
+        //            point = pricePoints[i];
+
+        //            if (i != pricePoints.Length - 1)
+        //            {
+        //                if (i != 0)//以前价格点
+        //                    _price.FillEllipse(redBrush, Convert.ToInt32(point.X - 3), Convert.ToInt32(point.Y - 3), 6, 6);
+        //                else//现在价格点
+        //                    _price.FillEllipse(redBrush, Convert.ToInt32(point.X - 8), Convert.ToInt32(point.Y - 8), 16, 16);
+        //                if (i % 5 == 0 && i != 0)//横坐标标记
+        //                    _price.DrawString(i.ToString(), bigFont, blackBrush, new Point(origin.X + i * unitVector.X - 12, origin.Y - priceGapNumber * unitVector.Y / 2 + 25));
+        //            }
+        //            else
+        //            {
+        //                _price.DrawString("轮次", smallFont, blackBrush, new Point(origin.X + i * unitVector.X - 12, origin.Y - priceGapNumber * unitVector.Y / 2 + 25));
+        //            }
+
+        //            //横坐标线
+        //            _price.DrawLine(blackPen, new Point(origin.X + i * unitVector.X, origin.Y - priceGapNumber * unitVector.Y / 2), new Point(origin.X + i * unitVector.X, origin.Y - priceGapNumber * unitVector.Y / 2 - (i % 5 == 0 ? 8 : 4)));
+        //        }
+        //    }
+        //    //本轮价格
+        //    _price.DrawString(string.Format("本轮价格：{0:F2}", priceList[priceList.Count - 1]), new Font(new FontFamily("Microsoft YaHei"), 20, FontStyle.Bold), blackBrush, new PointF(origin.X + priceList.Count * unitVector.X / 2 - 100, origin.Y - 200));
+        //}
         private void store()
         {
             StreamWriter _sw = new StreamWriter(_url);
-            _sw.Write(_price.WriteSVGString());
+            //_sw.Write(_price.WriteSVGString());
             _sw.Close();
         }
     }
--- Test/Program.cs
+++ Test/Program.cs
@@ -8,10 +8,9 @@ using CommonType;
 using DataIO.Mysql;
 using System.Collections;
 using Econophysics;
-using SvgNet;
-using SvgNet.SvgElements;
-using SvgNet.SvgTypes;
-using SvgNet.SvgGdi;
+using Svg;
+using Svg.Pathing;
+using Svg.DataTypes;
 
 namespace Test
 {
@@ -23,9 +22,106 @@ namespace Test
         }
         private static void testGraphic()
         {
-            SvgGraphics price = new SvgGraphics();
-            price.DrawLine(new Pen(Color.Black,2),new PointF(0,0),new PointF(10,10));
-            string x = price.ToString();
+            int _width = 900;
+            int _height = 500;
+            double[] priceRange = new double[2];
+            Point origin = new Point(_width - 50, _height / 2 - 30 / 2 + 20 / 2);
+            Point unitVector;
+            int priceGapNumber;
+            int basePrice;
+            List<double> priceList = new List<double>();
+            for (int i = 0; i < 50; i++)
+            {
+                priceList.Add(100*Math.Sin(i));
+            }
+            SvgColourServer blackPen = new SvgColourServer(Color.Black);
+            SvgUnit smallFont = new SvgUnit(SvgUnitType.Point, 16);
+            SvgUnit bigFont = new SvgUnit(SvgUnitType.Point, 18);
+            string fontFamily="微软雅黑";
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SvgColourServer redBrush = new SvgColourServer(Color.Red);
+
+            //获取最大最小价格
+            priceRange[0] = priceList[0];
+            priceRange[1] = priceList[0];
+            for (int i = 1; i < priceList.Count; i++)
+            {
+                priceRange[0] = (priceList[i] > priceRange[0] ? priceList[i] : priceRange[0]);
+                priceRange[1] = (priceList[i] < priceRange[1] ? priceList[i] : priceRange[1]);
+            }
+
+            //获取尺度
+            {
+                int tmp = Convert.ToInt32(priceRange[0] - priceRange[1]);
+                priceGapNumber = (tmp + tmp % 2 + 10);
+                priceGapNumber = (priceGapNumber > 20 ? priceGapNumber : 20);
+                tmp = Convert.ToInt32((priceRange[0] + priceRange[1]) / 2);
+                basePrice = tmp + (tmp % 5 > 2 ? 5 - tmp % 5 : -tmp % 5);
+            }
+            unitVector = new Point(-(_width - 57) / (priceList.Count - 1), -(_height - 50) / priceGapNumber);
+
+            SvgDocument s = new SvgDocument { Height = 500, Width = 900 };
+            var group=new SvgGroup();
+            s.Children.Add(group);
+
+            //纵坐标
+            group.Children.Add(new SvgLine { StartX = origin.X, StartY = 0, EndX = origin.X, EndY = _height, Stroke = blackPen,StrokeWidth=2 });
+            //横坐标
+            group.Children.Add(new SvgLine { StartX = 0, StartY = origin.Y - priceGapNumber * unitVector.Y / 2, EndX = _width, EndY = origin.Y - priceGapNumber * unitVector.Y / 2, Stroke = blackPen,StrokeWidth=2 });
+            group.Children.Add(new SvgText { Text = "价格", FontFamily = fontFamily, FontSize = smallFont, FontWeight = SvgFontWeight.bold, X = origin.X, Y = origin.Y - priceGapNumber * unitVector.Y / 2 + 25 });
+            //纵坐标标记
+            {
+                int tmp = priceGapNumber / 8;
+                tmp = tmp + 5 - tmp % 5;
+                for (int i = 0; i <= priceGapNumber / 2; i++)
+                {
+                    group.Children.Add(new SvgLine { StartX = origin.X - (i % 5 == 0 ? 8 : 4), StartY = origin.Y - i * unitVector.Y, EndX = origin.X, EndY = origin.Y - i * unitVector.Y, Stroke = blackPen ,StrokeWidth=2});
+                    group.Children.Add(new SvgLine { StartX = origin.X - (i % 5 == 0 ? 8 : 4), StartY = origin.Y + i * unitVector.Y, EndX = origin.X, EndY = origin.Y + i * unitVector.Y, Stroke = blackPen ,StrokeWidth=2});
+                  
+                    if (i % tmp == 0 && i != priceGapNumber / 2 && i != 0)
+                    {
+                        group.Children.Add(new SvgText { Text = (basePrice + i).ToString(), FontFamily = fontFamily, FontSize = bigFont, X = origin.X + 2, Y = origin.Y + i * unitVector.Y + 10 });
+                        group.Children.Add(new SvgText { Text = (basePrice - i).ToString(), FontFamily = fontFamily, FontSize = bigFont, X = origin.X + 2, Y = origin.Y - i * unitVector.Y + 10 });
+                    }
+                    else if (i == 0)
+                    {
+                        group.Children.Add(new SvgText { Text = basePrice.ToString(), FontFamily = fontFamily, FontSize = bigFont, X = origin.X + 2, Y = origin.Y + 10 });
+                    }
+                }
+            }
+            //价格线
+            SvgPolyline pricePath = new SvgPolyline { Fill=SvgPaintServer.None,Stroke = new SvgColourServer(Color.Blue), StrokeWidth = 2, Points = new SvgUnitCollection() };
+            group.Children.Add(pricePath);
+            for (int i = 1; i <= priceList.Count; i++)
+            {
+                pricePath.Points.Add(origin.X + i * unitVector.X);
+                pricePath.Points.Add((Single)(origin.Y + (priceList[priceList.Count - i] - basePrice) * unitVector.Y));
+            }
+
+            //标记点和价格
+                for (int i = 0; i < pricePath.Points.Count/2; i++)
+                {
+                    if (i != pricePath.Points.Count / 2 - 1)
+                    {
+                        if (i != 0)//以前价格点
+                            group.Children.Add(new SvgEllipse { CenterX = pricePath.Points[2 * i] - 3, CenterY = pricePath.Points[2 * i + 1] - 3, RadiusX = 6, RadiusY = 6, Fill = redBrush,Stroke=SvgPaintServer.None });
+                        else//现在价格点
+                            group.Children.Add(new SvgEllipse { CenterX = pricePath.Points[2 * i] - 8, CenterY = pricePath.Points[2 * i + 1] - 8, RadiusX = 16, RadiusY = 16, Fill = redBrush, Stroke = SvgPaintServer.None });
+                        if (i % 5 == 0 && i != 0)//横坐标标记
+                            group.Children.Add(new SvgText { Text = i.ToString(), FontFamily = fontFamily, FontSize = bigFont, X = origin.X + i * unitVector.X - 12, Y = origin.Y - priceGapNumber * unitVector.Y / 2 + 25 });
+                    }
+                    else
+                    {
+                        group.Children.Add(new SvgText { Text = "轮次", FontFamily = fontFamily, FontSize = smallFont, FontWeight = SvgFontWeight.bold, X = origin.X + i * unitVector.X - 12, Y = origin.Y - priceGapNumber * unitVector.Y / 2 + 25 });
+                    }
+
+                    //横坐标线
+                    group.Children.Add(new SvgLine { StartX = origin.X + i * unitVector.X, StartY = origin.Y - priceGapNumber * unitVector.Y / 2, EndX = origin.X + i * unitVector.X, EndY = origin.Y - priceGapNumber * unitVector.Y / 2 - (i % 5 == 0 ? 8 : 4), Stroke = blackPen, StrokeWidth = 2 });
+                }
+            //本轮价格
+                group.Children.Add(new SvgText { Text = string.Format("本轮价格：{0:F2}", priceList[priceList.Count - 1]), FontFamily = fontFamily, FontSize = new SvgUnit(SvgUnitType.Point, 20), FontWeight = SvgFontWeight.bold, X = origin.X + priceList.Count * unitVector.X / 2 - 100, Y = origin.Y - 200 });
+           
+            string x=s.GetXML();
         }
         private static void testExperiment()
         {
