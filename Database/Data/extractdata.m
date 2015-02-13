2b23528302c44a6a42e86f5bec13f707f7dfefb0
dfhljf <jianghui19940917@hotmail.com>
2015-02-13 21:16:29 +0800

    添加matlab函数
    Signed-off-by: dfhljf <jianghui19940917@hotmail.com>

---
+++ Database/Data/extractdata.m
@@ -0,0 +1,7 @@
+function [ parameters market agents ] = extractdata( expId )
+%UNTITLED 此处显示有关此函数的摘要
+%   此处显示详细说明
+
+
+end
+
---
+++ Database/myscript.m
@@ -0,0 +1,10 @@
+function myscript( path )
+%MYSCRIPT 执行给定的sql脚本文件
+%   脚本文件不返回数据集
+sql=importdata(path);
+
+for i=1:length(sql)
+    mysql(sql{i});
+end
+end
+
